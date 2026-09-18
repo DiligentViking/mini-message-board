ac7db1ae1542b9b06774502bd6921724ba5779d6
DiligentViking <acore-n@proton.me>
2026-09-18 12:07:40 -0500

    Try to fix deployment by adding host

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use((err, _, res, __) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, (error) => {
+app.listen(PORT, '0.0.0.0', (error) => {
   if (error) throw error;
   console.log(`Server running on port ${PORT}`);
 });
--- db/pool.js
+++ db/pool.js
@@ -1,5 +1,13 @@
 const { Pool } = require('pg');
 
+console.log({
+  host: process.env.PGHOST,
+  user: process.env.PGUSER,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
+});
+
 module.exports = new Pool({
   host: process.env.PGHOST,
   user: process.env.PGUSER,
